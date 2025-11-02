40427ac095067bdb39051ebbd481f1ffdd9421a6
amirreza <45253403+amirkhoshnejad@users.noreply.github.com>
2025-11-02 22:14:58 +0330

    v01

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,10 @@
+let num=0;
+
+const img1 = document.getElementById('image1');
+const img2 = document.getElementById('image2');
+
+if(num%2===0){
+    img2.style.display='block';
+}else{
+    img1.style.display='block';
+}
